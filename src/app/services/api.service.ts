cb9c4688bd588e4f25479d40b54188a38120ad40
dimitaracev <dimitaracev@protonmail.com>
2025-10-25 00:25:57 +0200

    [k8s] update baseUrl

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -10,7 +10,7 @@ import SignInRegisterResponse from '../responses/sign-in-register-response';
 export class ApiService {
   private http = inject(HttpClient);
 
-  private baseurl: string = 'http://3.73.152.93';
+  private baseurl: string = 'http://3.73.152.93/';
   private isRefreshingToken: boolean = false;
 
   private getAuthHeaders(): HttpHeaders {
